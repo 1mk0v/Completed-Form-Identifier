ce707e227ff20e9e5cb785141611da22146ad0c4
1mk0v <potapchuk01@mail.ru>
2023-11-15 15:52:08 +0300

    Add getTemplates func

--- src/main.py
+++ src/main.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI, Request 
 from validators import validateDict
+from utils import isSubsetList, getSubsetDict
 from Database.database import (
-    database as db,
-    templates
+    database as db
 )
+
 
 app = FastAPI(
     title='Completed Form Identifier'
 )
 
+def getTemplatesNames(template:dict, fromData:dict):
+    result = list()
+    for i in fromData:
+        validatedDBForm = validateDict(i)
+        if isSubsetList(validatedDBForm.keys(), template.keys()) \
+            and validatedDBForm == getSubsetDict(template, validatedDBForm):
+                result.append(i['name'])
+    if len(result) == 0:
+        result = template
+    return result
+
+
 @app.post('/get_form')
-async def getForm(
-    request:Request
-):
-    dictForm = dict(request.query_params)
-    validatedForm = validateDict(dictForm)
-    for i in db.all():
-        validatedDBElement = validateDict(i)
-        if set(validatedDBElement.keys()).issubset(set(validatedForm.keys())):
-            print("TRUE")
-    return dictForm
+async def getForm(request:Request):
+    responseForm = dict(request.query_params)
+    validateResponseForm = validateDict(responseForm)
+    return getTemplatesNames(validateResponseForm, db.all())
